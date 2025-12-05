a4e900a1901208e9ebeba38ac1b261284f83764a
Mukesh <149927808+mukkss@users.noreply.github.com>
2025-12-05 12:46:39 +0530

    Implement Candidate Elimination Algorithm in Lab1B.py

---
+++ Lab1B.py
@@ -0,0 +1,44 @@
+# CANDIDATE ELIMINATION ALGORITHM
+import numpy as np
+import pandas as pd
+
+
+data = pd.read_csv('enjoysport.csv')
+concepts = np.array(data.iloc[:, 0:-1])
+target = np.array(data.iloc[:, -1])
+
+
+def learn(concepts, target):
+    specific_h = concepts[0].copy()
+    general_h = [["?" for _ in range(len(specific_h))] for _ in range(len(specific_h))]
+
+
+    for i, h in enumerate(concepts):
+        if target[i] == "yes":
+            for x in range(len(specific_h)):
+                if h[x] != specific_h[x]:
+                    specific_h[x] = '?'
+                    general_h[x][x] = '?'
+
+
+        if target[i] == "no":
+            for x in range(len(specific_h)):
+                if h[x] != specific_h[x]:
+                    general_h[x][x] = specific_h[x]
+                else:
+                    general_h[x][x] = '?'
+
+
+    indices = [i for i, val in enumerate(general_h) if val == ['?'] * len(specific_h)]
+    for i in indices:
+        general_h.remove(['?'] * len(specific_h))
+
+
+    return specific_h, general_h
+
+
+s_final, g_final = learn(concepts, target)
+
+
+print("Final Specific_h:", s_final)
+print("Final General_h:", g_final)
